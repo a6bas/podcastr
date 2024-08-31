a00be257dd3e34714a1c60be9463e64216cf20c3
a6bas <aliabbas7213@gmail.com>
2024-08-31 04:03:13 +0200

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://suited-monster-9.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
